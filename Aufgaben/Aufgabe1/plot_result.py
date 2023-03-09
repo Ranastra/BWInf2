b3536702242ca3e901ff4db3bbdd41eee768e647
Ranastra <karo.schnitter@gmx.de>
2023-03-09 09:03:21 +0100

    added plot points

--- Aufgaben/Aufgabe1/plot_result.py
+++ Aufgaben/Aufgabe1/plot_result.py
@@ -16,8 +16,23 @@ def plot_path(i):
         plt.show()
         file.close()
 
+def plot_points(i):
+    with open("testcases/bsp" + str(i) + ".txt", "r") as file:
+        lines = [line.strip() for line in file.readlines()]
+        xs = []
+        ys = []
+        for point in lines[1:]:
+            x, y = point.split(" ")
+            xs.append(float(x))
+            ys.append(float(y))
+            #plt.plot(float(x), float(y))
+        plt.plot(xs, ys, marker=".", linewidth=0)
+        plt.suptitle(str(i))
+        plt.show()
+        file.close()
+
+
 
 if __name__ == "__main__":
     for i in range(1,8):
-        if i == 5: continue
-        else: plot_path(i)
+        plot_points(i)
