a9f3143657ed4f13e2392d57b98e03730819f160
Ranastra <karo.schnitter@gmx.de>
2023-03-26 22:31:08 +0200

    added args for fast and rand mode

--- Aufgaben/Aufgabe1/run.sh
+++ Aufgaben/Aufgabe1/run.sh
@@ -2,7 +2,7 @@
 
 path_to_input=$(pwd)/$1
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-.solve $path_to_input # rust program absoluter path + name
+./solve $path_to_input $2 $3 # rust program absoluter path + name
 if pip freeze | grep -q matplotlib; then
     python3 plot_result.py $1 # in script wird Rest des paths entfernt
 fi;
